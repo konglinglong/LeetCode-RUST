6f1c5655704dd9619a339c4590b4e1ce9494a9f5
konglinglong <konglinglong@gmail.com>
2019-02-26 14:57:59 +0800

    155. 最小栈

---
+++ src/min_stack.rs
@@ -0,0 +1,90 @@
+/// 设计一个支持 push，pop，top 操作，并能在常数时间内检索到最小元素的栈。
+/// 
+/// push(x) -- 将元素 x 推入栈中。
+/// pop() -- 删除栈顶的元素。
+/// top() -- 获取栈顶元素。
+/// getMin() -- 检索栈中的最小元素。
+/// 
+/// 示例:
+/// 
+/// MinStack minStack = new MinStack();
+/// minStack.push(-2);
+/// minStack.push(0);
+/// minStack.push(-3);
+/// minStack.getMin();   --> 返回 -3.
+/// minStack.pop();
+/// minStack.top();      --> 返回 0.
+/// minStack.getMin();   --> 返回 -2.
+
+#[derive(Debug)]
+struct StackValue {
+    value: i32,
+    minValue: i32,
+}
+
+#[derive(Debug)]
+struct MinStack {
+    stack: Vec<StackValue>,
+}
+
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl MinStack {
+
+    /** initialize your data structure here. */
+    fn new() -> Self {
+        Self {
+            stack: Vec::<StackValue>::new(),
+        }
+    }
+
+    fn push(&mut self, x: i32) {
+        let mut min: i32 = 0;
+        if self.stack.len() > 0 && self.stack[self.stack.len() - 1].minValue < x {
+            min = self.stack[self.stack.len() - 1].minValue;
+        } else {
+            min = x;
+        }
+        self.stack.push(StackValue{value: x, minValue: min});
+    }
+
+    fn pop(&mut self) {
+        self.stack.pop();
+    }
+
+    fn top(&mut self) -> i32 {
+        if self.stack.len() > 0 {
+            self.stack[self.stack.len() - 1].value
+        } else {
+            0
+        }
+    }
+
+    fn get_min(&self) -> i32 {
+        if self.stack.len() > 0 {
+            self.stack[self.stack.len() - 1].minValue
+        } else {
+            0
+        }
+    }
+}
+
+#[cfg(test)]
+mod test
+{
+    use super::*;
+
+    #[test]
+    fn test_min_stack()
+    {
+        let mut stack = MinStack::new();
+        stack.push(-2);
+        stack.push(0);
+        stack.push(-3);
+        println!("stack {:?}", stack);
+//        assert_eq!(compare_version("0.1".to_string(), "1.1".to_string()), -1);
+//        assert_eq!(compare_version("1.0.1".to_string(), "1.0".to_string()), 1);
+    }
+}
